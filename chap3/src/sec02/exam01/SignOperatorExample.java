0c8cf1b69ee1ec296937662988c4d2f4fe20ff62
Ksk611 <oppllii@naver.com>
2023-07-28 17:26:13 +0900

    0728

---
+++ chap3/src/sec02/exam01/SignOperatorExample.java
@@ -0,0 +1,24 @@
+package sec02.exam01;
+
+public class SignOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = -100;
+		int result1 = +x;
+		int result2 = -x;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		byte y = -100;
+		//byte result3 = +y; 에러
+		int result3 = -y;
+		System.out.println(result3);
+		
+				
+		
+
+	}
+
+}
